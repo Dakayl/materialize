9303ac2ea9fbaa95d5f20bb882426741140450ce
Nicolas Brouet <n.brouet@genesis-groupe.com>
2018-03-15 15:25:38 +0100

    modif package

--- package.js
+++ package.js
@@ -3,8 +3,8 @@
 Package.describe({
   name: 'materialize:materialize',  // http://atmospherejs.com/materialize/materialize
   summary: 'Materialize (official): A modern responsive front-end framework based on Material Design',
-  version: '0.100.2',
-  git: 'https://github.com/Dogfalo/materialize.git'
+  version: '0.101.0',
+  git: 'https://github.com/Dakayl/materialize.git'
 });
 
 
